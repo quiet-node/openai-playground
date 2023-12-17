77eb80970c9dddcb05020e4ade0cedbefd786ac3
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-12-16 22:50:05 -0500

    feat: added new API to generate dataset
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- server/routes/api/openai-api.ts
+++ server/routes/api/openai-api.ts
@@ -1,6 +1,8 @@
+import fs from 'fs';
 import 'dotenv/config';
 import OpenAI from 'openai';
 import express from 'express';
+import upload from '../../middleware/file-upload';
 
 const openAIRouter = express.Router();
 
@@ -11,7 +13,7 @@ const openai = new OpenAI({
 openAIRouter.post('/chat-completions', async (req, res) => {
   const content = req.body.content;
   const stream = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo-16k',
+    model: 'gpt-3.5-turbo-1106',
     messages: [
       {
         role: 'system',
@@ -27,6 +29,8 @@ openAIRouter.post('/chat-completions', async (req, res) => {
     process.stdout.write(chunk.choices[0]?.delta?.content || '');
   }
   console.log();
+
+  res.send(stream);
 });
 
 openAIRouter.post('/creater-new-assistant', async (req, res) => {
@@ -75,5 +79,48 @@ openAIRouter.post('/talk-to-assistant', async (req, res) => {
   const completed_messages = await openai.beta.threads.messages.list(threadId);
   res.send(completed_messages.data);
 });
+
+openAIRouter.post(
+  '/generate-dataset',
+  upload.single('hedera-docs'),
+  async (req, res) => {
+    const file = req.file;
+    const pairNum = req.body.pairNum;
+    const fileContent = fs.readFileSync(file!.path, 'ascii');
+
+    const prompt = `Please regard to this "information base" here:
+\`\`\`
+${fileContent}
+\`\`\`
+Generate a list of at least ${pairNum} Q&A pairs. REMEMBER THAT the list has at least ${pairNum} pairs. REMEMBER THAT I want the Q&A pairs to cover all the information provided by the file. `;
+
+    const stream = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo-1106',
+      messages: [
+        {
+          role: 'system',
+          content:
+            'You are professional at making questions and answers by examinating the information base provided by user. REMEBER TO focus on Hedera network items. REMEMBER THAT each pair of Q&A is distributed in an object like this: {"messages": [{"role": "user", "content": [question goes here]}, {"role": "assistant", "content": [answers goes here ]}]}. REMEMBER THAT for each object, please stringify it and apply no format at all. REMEMBER THAT the object should have NO EXTRA SPACE or DO NOT break down to a new line.',
+        },
+        { role: 'user', content: prompt },
+      ],
+      stream: true,
+    });
+
+    const datasetFileName = `dataset/${file!.originalname.replace(
+      '.md',
+      ''
+    )}.jsonl`;
+
+    var writeStream = fs.createWriteStream(datasetFileName);
+    for await (const chunk of stream) {
+      process.stdout.write(chunk.choices[0]?.delta?.content || '');
+      writeStream.write(chunk.choices[0]?.delta?.content || '');
+    }
+    writeStream.end();
+    console.log('\nDone.\n');
+    res.send(pairNum);
+  }
+);
 
 export default openAIRouter;
